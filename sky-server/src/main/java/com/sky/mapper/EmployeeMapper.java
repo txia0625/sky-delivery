6b607f4df41538e0036600c731b8c7f6eb51606b
Tong Xia <gregxia0625@gmail.com>
2024-04-15 13:39:04 -0500

    fix: correct SQL query for inserting a new employee

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -16,8 +16,8 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
-    @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user)"+
-    "values" +
+    @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user, status) "+
+    "values " +
     "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
 
     void insert(Employee employee);
