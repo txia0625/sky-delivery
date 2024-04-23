62342fdd85a194a1ab2f6e00572d04d3092ba788
Tong Xia <gregxia0625@gmail.com>
2024-04-23 16:52:12 -0500

    fix: fix DishController

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -4,8 +4,10 @@ import com.sky.dto.DishDTO;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,9 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
+
+    @PostMapping
+    @ApiOperation("Add new dish")
     public Result save(@RequestBody DishDTO dishDTO)
     {
         log.info("Newly Added Dish: {}", dishDTO);
