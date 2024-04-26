1ce2621e64e6edf2581c7143206d9e777c3d2cf6
Tong Xia <gregxia0625@gmail.com>
2024-04-26 18:02:46 -0500

    feat: implemented dish deletion

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin/dish")
 @Api(tags = "Dish Related APIs")
@@ -37,5 +39,15 @@ public class DishController {
         log.info("Dish Page Query: {}", dishPageQueryDTO);
         PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    @DeleteMapping
+    @ApiOperation("Dish Batch Deleting")
+    public Result delete(@RequestParam List<Long> ids)
+    {
+        log.info("Dish Batch Deleting: {}", ids);
+        dishService.deleteBatch(ids);
+        return Result.success();
+
     }
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -4,7 +4,11 @@ import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.result.PageResult;
 
+import java.util.List;
+
 public interface DishService {
     public void saveWithFlavor(DishDTO dishDTO);
     public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    void deleteBatch(List<Long> ids);
 }
